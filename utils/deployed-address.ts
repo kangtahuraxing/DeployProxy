e9782184cc5f451e06c8edd658294b383658181e
kangtahuraxing . <cuanbgt263@gmail.com>
2024-08-05 18:12:52 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x6EB3B056a2dC75A379228e1f1D010B401d87f66B'
+export const ProxyAdmin = '0xDa91dE92dD74B8665b6C195ae8185eB90B3eBf53'
+export const SWTRImplementation = '0xb28d909294f94231f1AC377E8687Dba15aAd5027'
     
